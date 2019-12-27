7c87505d26e5c99bc549def788ef240f54fae50d
Goker <gokerakce@outlook.com>
2019-12-27 22:18:38 +0000

    Added react routing

--- src/App.js
+++ src/App.js
@@ -1,13 +1,26 @@
 import React, { Component } from 'react';
 import Movies from './components/movies';
-import 'bootstrap/dist/css/bootstrap.css';
-import 'font-awesome/css/font-awesome.css'
+import NavBar from './components/common/navBar';
+import {Route, Redirect, Switch} from 'react-router-dom';
+import Customers from './components/customers';
+import Rentals from './components/rentals';
+import NotFound from './components/notFound';
 
 class App extends Component {
   render() { 
     return (
       <React.Fragment>
-        <Movies />
+      <NavBar />
+      <main className="container">
+        <Switch>
+          <Route path="/customers" component={Customers} />
+          <Route path="/movies" component={Movies} />
+          <Route path="/rentals" component={Rentals}></Route>
+          <Route path="/not-found" component={NotFound} />
+          <Redirect from="/" to="/movies" />
+          <Redirect to="/not-found" />
+        </Switch>
+      </main>
       </React.Fragment>
       );
   }
