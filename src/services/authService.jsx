b9205576c169d51917ca4c92851e2efb310245fe
Goker <gokerakce@outlook.com>
2020-01-05 01:19:33 +0000

    Added auth service

---
+++ src/services/authService.jsx
@@ -0,0 +1,9 @@
+import http from './httpService';
+import { apiURL } from '../config.json'
+
+const apiEndPoint = apiURL + "/auth";
+
+
+export function login(email, password) {
+  return http.post(apiEndPoint, { email, password });
+}
