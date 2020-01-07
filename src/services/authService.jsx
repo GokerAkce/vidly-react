6f47894abca546db8d988278c2ef9e1cf672372e
Goker <gokerakce@outlook.com>
2020-01-07 21:41:33 +0000

    Added jwt to http request header

--- src/services/authService.jsx
+++ src/services/authService.jsx
@@ -5,6 +5,8 @@ import jwtDecode from 'jwt-decode';
 const apiEndPoint = apiURL + "/auth";
 const tokenKey = "token";
 
+http.setJwt(getJwt());
+
 export async function login(email, password) {
   const {data: jwt} = await http.post(apiEndPoint, { email, password });
   localStorage.setItem(tokenKey, jwt);
@@ -25,6 +27,10 @@ export function getCurrentUser(){
 
 export function logout(){
   localStorage.removeItem(tokenKey);
+}
+
+function getJwt(){
+  return localStorage.getItem(tokenKey);
 }
 
 export default {
